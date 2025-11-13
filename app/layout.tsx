4ab8d5e5ecb77638a161350a17402dd6123139b3
Prof Douglas <you@example.com>
2025-11-13 12:15:41 +0000

    Fix iOS notch background to match header

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Disable zooming */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+
+        {/* Disable text auto-enlargement */}
+        <meta name="text-size-adjust" content="100%" />
+      </head>
+
       <body>
         <Header />
         <main className="min-h-[calc(100dvh-56px)] pb-16">{children}</main>
@@ -31,5 +42,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ToastHost />
       </body>
     </html>
-  )
+  );
 }
+
